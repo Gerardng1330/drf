92e51808a6e8bc2856a422edc513a92279118baf
gerar <gerar132000ng@gmail.com>
2024-07-27 00:55:53 -0500

    change:urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,16 @@
 from rest_framework import routers
 from .api import ProjectViewSet
+from . import views
+from django.urls import path
+
+
 
 router= routers.DefaultRouter()
 router.register('api/projects', ProjectViewSet,'projects')
 
 urlpatterns = router.urls
+
+urlpatterns = [
+    #agregar y elminar cartas
+    path('prueba/', views.prueba, name='prueba'),
+]
